9d535ace593e8c850c8eb7d427bf7956f7048c29
Reuben Fletcher-Costin <reuben.fletchercostin@gmail.com>
2012-11-25 14:29:47 +1100

    fix bug in ignore_intervention_entirely_assumption

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def ignore_intervention_entirely_assumption(x, y, z, w, g):
     g_xbar = g.prune_edges_to(x)
     w_ancestors = find_ancestors(set(w), g.make_arcs_from())
     zw = set(vertex for vertex in z if vertex not in w_ancestors)
-    g_xbar_zwbar = g_xbar.prune_edges_from(zw)
+    g_xbar_zwbar = g_xbar.prune_edges_to(zw)
     arcs_from = g_xbar_zwbar.make_arcs_from()
     return (y and z and not is_d_connected(y, z, set(w) | set(x), arcs_from))
 
