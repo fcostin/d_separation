f076ea7de82ae575cab406cad07187df3f0448ad
Reuben Fletcher-Costin <reuben.fletchercostin@gmail.com>
2012-11-26 23:36:14 +1100

    add cute graphviz rendering of proof trees

--- Makefile
+++ Makefile
@@ -1,7 +1,22 @@
+GREED := 1e-9
+
+graph: proof_tree.svg
+
 test:
 	py.test --cov=. --cov-report=html .
+
+
+proof_tree.dot:	main.py
+	python $^ $(GREED)
+
+proof_tree.svg:	proof_tree.dot
+	unflatten -l 1 $^ | dot -Grankdir=LR -Tsvg -o $@
 
 clean:
 	rm -rf *.pyc
 	rm -rf __pycache__
 	rm -rf htmlcov
+	rm -rf *.svg
+	rm -rf *.dot
+
+.PHONY: test graph clean
--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import expr as E
 from test_rules import make_toy_graph
 from proof_state import ProofState
 from prover import (make_heuristic, proof_search, pleasantly_fmt)
+import sys
 
 def main():
+
+    if len(sys.argv) != 2:
+        sys.stderr.write('usage: greediness (positive float...)\n')
+        sys.exit(1)
+
+    greed = float(sys.argv[1])
+
     graph = make_toy_graph()
 
     banned_values = set([frozenset(['h'])])
     # dial the greed parameter up high.
     # this makes the search very optimistic.
     # in general this may not find the shortest proof
-    heuristic = make_heuristic(banned_values, greed=10)
+    heuristic = make_heuristic(banned_values, greed)
 
     initial_bindings = {
         'x' : frozenset(['x']),
@@ -38,9 +46,67 @@ def main():
     assert result['reached_goal']
     print 'success!'
 
-    display_proof(result['path'])
+    display_proof_as_listing(result['path'])
 
-def display_proof(proof_state):
+    out_file_name = 'proof_tree.dot'
+    write_proof_tree(result['path'], result['closed'], out_file_name)
+
+def write_proof_tree(path, closed, out_file_name):
+
+    proof = extract_proof(path)
+    solution_states = set(proof_state.extract_state() for proof_state in proof)
+
+    def make_fancy_label(proof_state):
+        s = pleasantly_fmt(proof_state.bindings, proof_state.root_expr)
+        rewrites = {
+            'sigma' : '&#8721;',
+            'product' : '&#8719;',
+            "'" : '&#8242;',
+        }
+        for src, dst in rewrites.iteritems():
+            s = s.replace(src, dst)
+        return s
+
+    with open(out_file_name, 'w') as f_out:
+        def put(s):
+            f_out.write(s + '\n')
+        def emit_node(i, proof_state):
+            label = make_fancy_label(proof_state)
+            style = {
+                'shape' : 'box',
+                'style' : 'rounded,filled',
+                'label' : label,
+            }
+            if proof_state.extract_state() in solution_states:
+                style['fillcolor'] = 'yellow'
+            else:
+                style['fillcolor'] = 'grey'
+            style_string = ','.join('%s="%s"' % (k, v) for (k, v) in style.iteritems())
+            put('\t%s [%s];' % (i, style_string))
+        def emit_arc(i, j):
+            put('\t%s -> %s;' % (i, j))
+        put('digraph proof_tree {')
+        extract_proof_tree(closed, emit_node, emit_arc)
+        put('}')
+
+def extract_proof_tree(closed, emit_node, emit_arc):
+    states = set()
+    for state in closed:
+        states.add(state)
+    states = list(sorted(states))
+
+    state_indices = {state:i for i, state in enumerate(states)}
+
+    for state in states:
+        i = state_indices[state]
+        proof_state = closed[state]
+        emit_node(i, proof_state)
+        if proof_state.parent is not None:
+            j = state_indices[proof_state.parent.extract_state()]
+            emit_arc(j, i)
+
+
+def display_proof_as_listing(proof_state):
     proof = extract_proof(proof_state)
     for i, proof_state in enumerate(proof):
         print 'step %d' % i
